7996710df62a70e9030687802ebe9320de5af25e
Jeong Hun Moon <wjdgns8243@naver.com>
2022-09-18 22:15:03 +0900

    init

---
+++ app.py
@@ -0,0 +1,28 @@
+import os 
+import subprocess as sp
+from flask import Flask, redirect, url_for, render_template, request
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/ON')
+def ON():
+	#process = sp.Popen('/home/pi/SmartUmb/main.py')
+	os.system("python3 main.py")
+	return redirect(url_for('home'))
+	
+@app.route('/OFF')
+def OFF():
+	#os.system('taskkill.exe /f /im main.py')
+	os.system('killall -9 main.exe')
+	
+	print("exit")
+	return redirect(url_for('home'))
+	
+if __name__ == '__main__':
+	print("server  Start")
+	app.run()
---
+++ main.py
@@ -0,0 +1,6 @@
+while True:
+	print("hello")
+	# Multi Thread are maked 
+	#Thread1 detect the weight on the loadCell 
+	#Thread2, Thread2 bring the wheather Data every 3 hours
+
